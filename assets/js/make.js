8997ee38753cae9800ecdec148d6d8696dc96f46
Shayane Umar <sumar@shoretel.com>
2017-12-30 23:43:10 +0530

    Move static contents to S3

--- assets/js/make.js
+++ assets/js/make.js
@@ -15,7 +15,7 @@ function loadImages () {
   $.each(toppings, function (key, val) {
     var img = new Image();
     img.setAttribute('crossOrigin', 'anonymous');
-    img.src = '/assets/toppings/' + val.image;
+    img.src = '//s3.ap-south-1.amazonaws.com/pizza-luvrs-shayane-umar/toppings/' + val.image;
     val.img = img;
   });
 }
